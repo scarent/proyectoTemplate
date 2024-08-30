c3f1ebd2c4026498622b3707d2af37fbf7bfc2b0
scarent <jorgeee.g.a@gmail.com>
2024-08-30 12:28:50 -0400

    templates
    se aprendio a hacer templates en las views y mostrar con archivo html

--- appTemplate/views.py
+++ appTemplate/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def rendertemplate(request):
+    return render(request,'appTemplate/holamundo.html') #nombreapp/archivohtml
+
+def saludojorge(request):
+    data={"nombre" : "Jorge"}
+    return render(request,'appTemplate/holapol.html',data)
+
+def infoUsuario(request):
+    datausuario = {"ID" :123, "Nombre" : "Clark Kent", "Email": "superman@jl.org"}
+    return render(request,'appTemplate/infousuario.html',datausuario)
+
--- proyectoTemplate/urls.py
+++ proyectoTemplate/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appTemplate.views import rendertemplate,saludojorge,infoUsuario
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('render/',rendertemplate),
+    path('render2/',saludojorge),
+    path('infousuario/',infoUsuario)
 ]
